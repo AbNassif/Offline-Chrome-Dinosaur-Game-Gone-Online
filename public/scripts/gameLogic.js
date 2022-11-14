e0f0e5fb358a2a53a00ae6cab603f3f2e7c45c70
unknown <abnassif99@gmail.com>
2022-11-14 22:22:43 +0200

    Adding jumping functionality to dino

--- public/scripts/gameLogic.js
+++ public/scripts/gameLogic.js
@@ -1,8 +1,14 @@
-let defaultWalk = "./images/game_assets/characters/yellowDigimon/default.png"
-let walkOneFront = "./images/game_assets/characters/yellowDigimon/walkOneFront.png"
-let walkTwoFront= "./images/game_assets/characters/yellowDigimon/walkTwoFront.png"
-let walkOneBack= "./images/game_assets/characters/yellowDigimon/walkOneBack.png"
-let walkTwoBack = "./images/game_assets/characters/yellowDigimon/walkTwoBack.png"
+//Character Assets
+const defaultWalk = "./images/game_assets/characters/yellowDigimon/default.png"
+const walkOneFront = "./images/game_assets/characters/yellowDigimon/walkOneFront.png"
+const walkTwoFront= "./images/game_assets/characters/yellowDigimon/walkTwoFront.png"
+const walkOneBack= "./images/game_assets/characters/yellowDigimon/walkOneBack.png"
+const walkTwoBack = "./images/game_assets/characters/yellowDigimon/walkTwoBack.png"
+const jumpSpriteImg = "./images/game_assets/characters/yellowDigimon/jump.png"
+
+//Obstacle Assets
+const singleCactus = "./images/game_assets/environment/cactus.png";
+const multipleCactueses = "./images/game_assets/environment/cactuses.png";
 
 
 //Using an object to pass by reference
@@ -13,10 +19,9 @@ let spriteArr = [defaultWalk, walkOneFront, walkTwoFront, walkOneFront,
     defaultWalk, walkOneBack, walkTwoBack, walkOneBack];
 let speed = 1;
 
-let spriteAnimation = (characterElement,...sprites) => {
-    characterElement.style.backgroundImage = `url(${spriteArr[
-        keepWithinBoundsOfArray(spriteArr, spriteInfo)]})`; 
-    console.log(characterElement.style.backgroundImage)
+let spriteAnimation = (characterElement,sprites) => {
+    characterElement.src = spriteArr[
+        keepWithinBoundsOfArray(spriteArr, spriteInfo)]
 }
 
 function keepWithinBoundsOfArray(arr, ind) {
@@ -26,8 +31,54 @@ function keepWithinBoundsOfArray(arr, ind) {
     return ind.currSprite= ind.currSprite + 1;
 }
 
-spriteAnimation(document.querySelector(".player"), spriteArr);
-spriteAnimation(document.querySelector(".player"), spriteArr);
-spriteAnimation(document.querySelector(".player"), spriteArr);
+// spriteAnimation(document.querySelector(".player"), spriteArr);
+// spriteAnimation(document.querySelector(".player"), spriteArr);
+// spriteAnimation(document.querySelector(".player"), spriteArr);
 
-const test = setInterval(spriteAnimation.bind(this,document.querySelector(".player"), spriteArr),500)
+let spriteAnimationInterval = setInterval(
+    spriteAnimation.bind(this, document.querySelector(".player"), spriteArr
+    ), 30)
+
+const spawnObstacles = (obstacleImgSrc) =>{
+
+}
+
+function jumpSprite(spriteAnimationInterval, sprite) {
+    let holdInterval = spriteAnimationInterval;
+    //cancel the sprite Animation
+    clearInterval(spriteAnimationInterval)
+
+    //add jump sprite
+    //add spriteAnimationAgain
+    spriteAnimationInterval = holdInterval;
+}
+
+function jump() {
+    let jumpingInProcess = false;
+    const player = document.querySelector(".player");  
+
+    //Listen for key pressed to jump
+    window.addEventListener("keydown", (e) => {
+        const key = e.code.toLowerCase();
+        if (key === "space") {
+            //Only jump when you're not already in the process of jumping
+            if (!jumpingInProcess) {
+                clearInterval(spriteAnimationInterval)
+                player.src = jumpSpriteImg;
+                player.style.animation = "jump 0.75s linear ";   
+                jumpingInProcess = true;
+            }
+        }
+    })
+
+    player.addEventListener("animationend", () => {
+        //Empty the animation to run it again later
+        player.style.animation = "";
+        jumpingInProcess = false;
+        spriteAnimationInterval = setInterval(
+        spriteAnimation.bind(this, document.querySelector(".player"), spriteArr
+    ), 30)
+    })
+
+}
+jump()
